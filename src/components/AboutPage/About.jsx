be85299a5230909165504726686ac93b4e0c0518
Laman Ismayilova <laman.mamedova98@gmail.com>
2024-01-05 15:19:27 -0800

    up

--- src/components/AboutPage/About.jsx
+++ src/components/AboutPage/About.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import styles from "./About.module.css"
 const About=()=> {
   return (
-  <div>Hey</div>
+  <div className={styles.info}>
+    <div className={styles.about_me}>
+      <p>Hey! I'm Laman Ismayilova. I'm a Full-Stack Web Developer who is passionated about JavaScript programming language and interested in ReactJS. Have experience in object-oriented programming, developing, testing, debugging code, designing interfaces. Quickly learning new technologies, successfully working in both team and self-directed settings. During my leisure hours prefer to play video/board games, watch anime with my spouse and read some classic literature.  </p>
+    </div>
+  </div>
   )
 }
 
